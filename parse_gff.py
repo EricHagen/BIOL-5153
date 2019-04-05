598e75b35032d7597d740702ac892d8c99c7dc71
Eric Hagen <erichagen1@Erics-MacBook-Pro-3.local>
2019-04-05 00:14:02 -0500

    assignment for Friday

--- parse_gff.py
+++ parse_gff.py
@@ -1,17 +1,38 @@
 #! usr/bin/env python3
 
+import argparse
+
+#Create an argument parser object ('parser') that will hold all the information
+#necessary to parse the command line
+parser = argparse.ArgumentParser(description = "generates a PBS commandline script for running this code")
+
+#add positional arguments, i.e., the required input
+parser.add_argument("dna_file", help = "path to the file that holds your DNA sequence")
+parser.add_argument("info_file", help = "path to the file that holds your exon & intron information")
+
+#Parse the command line arguments
+args = parser.parse_args()
+
+#Set paths to files to run
+fsa_path = "/Users/erichagen1/Desktop/University_of_Arkansas_Stuff/Spring_2019_Semester/Practical_Programming/watermelon_files/"
+gff_path = "/Users/erichagen1/Desktop/University_of_Arkansas_Stuff/Spring_2019_Semester/Practical_Programming/watermelon_files/"
+
+#Designate dna & info files to run
+fsa_file = fsa_path + args.dna_file
+gff_file = gff_path + args.info_file
+
 #Read in watermelon fsa & gff
-with open("/Users/erichagen1/Desktop/University_of_Arkansas_Stuff/Spring_2019_Semester/Practical_Programming/watermelon_files/watermelon.fsa") as open_fsa:
+with open(fsa_file) as open_fsa:
     fsa = open_fsa.read().splitlines()
-gff = open("/Users/erichagen1/Desktop/University_of_Arkansas_Stuff/Spring_2019_Semester/Practical_Programming/watermelon_files/watermelon.gff", "r")
+gff = open(gff_file, "r")
 
 #Print just the fsa genome
-print("\nWATERMELON FSA GENOME:\n")
+print("\nGENOME:\n")
 lines = fsa[1:]
 print(lines)
 
 #Reopen the fsa file for use in the second part:
-fsa2 = open("/Users/erichagen1/Desktop/University_of_Arkansas_Stuff/Spring_2019_Semester/Practical_Programming/watermelon_files/watermelon.fsa").read()
+fsa2 = open(fsa_file).read()
 
 #Write a function to calculate GC content
 def get_GC_content(sequence, sigfigs = 2):
@@ -21,7 +42,6 @@ def get_GC_content(sequence, sigfigs = 2):
 	return round(gc_content, sigfigs)
 
 #Parse files & extract all features of genome
-startsandstops = []
 print("\nGC CONTENT OF SEQUENCES:")
 seqnumber = 1
 for line in gff:
@@ -31,7 +51,8 @@ for line in gff:
 	start = int(fields[3]) - 1
 	stop = int(fields[4]) - 1
 	output = fsa2[start:stop]
-	startsandstops.append(output)
-	print("Sequence", seqnumber, "~", get_GC_content(output))
+	name = fields[8]
+	name_characters = name[0:10]
+	print("Sequence", seqnumber, "a.k.a. " + name_characters + " ~", get_GC_content(output))
 	seqnumber = seqnumber + 1
 gff.close()
