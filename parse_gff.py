4c7f90f46c478a48096fba3f34207c0c8d4f5991
Eric Hagen <erichagen1@erics-mbp-3.ddns.uark.edu>
2019-04-15 12:21:40 -0500

    Assn 07: Included functions for all the major tasks completed by the script

--- parse_gff.py
+++ parse_gff.py
@@ -2,59 +2,91 @@
 
 import argparse
 import csv
-
-#Create an argument parser object ('parser') that will hold all the information
-#necessary to parse the command line
-parser = argparse.ArgumentParser(description = "generates a PBS commandline script for running this code")
-
-#add positional arguments, i.e., the required input
-parser.add_argument("dna_file", help = "path to the file that holds your DNA sequence")
-parser.add_argument("info_file", help = "path to the file that holds your exon & intron information")
-
-#Parse the command line arguments
-args = parser.parse_args()
+from Bio import SeqIO
 
 #Set paths to files to run
 fsa_path = "/Users/erichagen1/Desktop/University_of_Arkansas_Stuff/Spring_2019_Semester/Practical_Programming/watermelon_files/"
 gff_path = "/Users/erichagen1/Desktop/University_of_Arkansas_Stuff/Spring_2019_Semester/Practical_Programming/watermelon_files/"
 
+#Write a function to designate arguments
+def get_args():
+	#Create an argument parser object ('parser') that will hold all the information
+	#necessary to parse the command line
+	parser = argparse.ArgumentParser(description = "generates a PBS commandline script for running this code")
+	#add positional arguments, i.e., the required input
+	parser.add_argument("dna_file", help = "path to the file that holds your DNA sequence")
+	parser.add_argument("info_file", help = "path to the file that holds your exon & intron information")
+	#Parse the command line arguments
+	return parser.parse_args()
+
+#Write a function to read the GFF file
+def read_gff(gff_file):
+	gff = open(gff_file, "r")
+	return gff
+
+#Write a function to parse the GFF file
+def parse_the_gff(gff):
+	csvreader = csv.reader(gff, delimiter = "\t")
+	return csvreader
+
+#Write a function to read the fsa file
+def read_fsa(fsa_file):
+	#Use SeqIO to open the fsa file
+	fsa = SeqIO.read(fsa_file, 'fasta')
+	#Print the fasta file
+	print("\nGENOME:")
+	print(fsa) #Use fsa.seq to print the entire sequence
+	return fsa.seq
+
+#Write a function to calculate GC content
+def get_GC_content(sequence, sigfigs = 2):
+	g_content = sequence.count('G')
+	c_content = sequence.count('C')
+	gc_content = (g_content + c_content) / len(sequence)
+	return round(gc_content, sigfigs)
+
+#Write a function to get the reverse complement of a sequence
+def reverse__complement(sequence):
+	reverse = sequence.reverse_complement()
+	return reverse
+
+#Get GC content values
+def print_GC_content(fsa, csvreader):
+	print("\nGC CONTENT OF SEQUENCES:")
+	seqnumber = 1
+	for row in csvreader:
+		start = int(row[3]) - 1
+		stop = int(row[4]) - 1
+		output = fsa[start:stop]
+		name = row[8]
+		name_characters = name[0:10]
+		print("Sequence", seqnumber, "a.k.a. " + name_characters + " ~", get_GC_content(output))
+		if row[6] == "-":
+			start = int(row[3]) - 1
+			stop = int(row[4]) - 1
+			output = fsa[start:stop]
+			c = reverse__complement(output)
+			print("This is a minus strand, and its reverse complement is:")
+			print(c)
+			print("\n")
+		else:
+			print("This is a + strand\n")
+		seqnumber = seqnumber + 1
+
+#Write the main function through which the whole program will be executed
+def main():
+	gff = read_gff(gff_file)
+	csvreader = parse_the_gff(gff)
+	fsa = read_fsa(fsa_file)
+	print_GC_content(fsa, csvreader)
+
+#Call the function we defined above
+args = get_args()
+
 #Designate dna & info files to run
 fsa_file = fsa_path + args.dna_file
 gff_file = gff_path + args.info_file
 
-#Open gff file
-with open(gff_file, 'r') as gff:
-
-	#Create a csv reader object to open the gff file
-	csvreader = csv.reader(gff, delimiter = "\t")
-
-	#Import the SeqIO module & open the fsa file with it
-	from Bio import SeqIO
-	fsa = SeqIO.read(fsa_file, 'fasta')
-
-	#Print the fasta file
-	print("\nGENOME:")
-	print(fsa)
-
-	#Write a function to calculate GC content
-	def get_GC_content(sequence, sigfigs = 2):
-		g_content = sequence.count('G')
-		c_content = sequence.count('C')
-		gc_content = (g_content + c_content) / len(sequence)
-		return round(gc_content, sigfigs)
-
-	#Get GC content values
-	print("\nGC CONTENT OF SEQUENCES:")
-	seqnumber = 1
-	fullsequence = fsa.seq
-	for row in csvreader:
-		start = int(row[3]) - 1
-		stop = int(row[4]) - 1
-		output = fullsequence[start:stop]
-		name = row[8]
-		name_characters = name[0:10]
-		print("Sequence", seqnumber, "a.k.a. " + name_characters + " ~", get_GC_content(output))
-		seqnumber = seqnumber + 1
-
-#Close the gff file
-gff.close()
+#Execute the program by calling the main function
+if __name__ == "__main__":
+	main()
