4738a242d323eb5ef1299ef2a2499db61374fb66
Eric Hagen <erichagen1@Erics-MacBook-Pro-3.local>
2019-04-23 22:46:19 -0500

    ASSN 08: Regular expression to find variations of 'Katherine'

--- katharine.py
+++ katharine.py
@@ -3,8 +3,10 @@
 import re
 
 #Regular expression to match every variant of a name (HOMEWORK ASSIGNMENT 8):
-name = 'katharine'
-#other names: Katherine, Catheryn, Cathryn, Kathryn, Katherine, Catherine, & Kathrin
-match = re.search("[KC]ath([ae]*)r[iy]n(e*)", name, re.I)
+name = 'Kathrin'
+#names: katharine, Katherine, Catheryn, Cathryn, Kathryn, Katherine, Catherine, & Kathrin
+match = re.search("[KC]ath[ae]*r[iy]ne*", name, re.I)
 if match:
 	print("Found a match: ", match.group())
+else:
+	print("No match found.")
